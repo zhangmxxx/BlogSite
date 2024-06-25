cdf86d5462d507b056162a994343f8893b7b3afd
Simenons <211240005@smail.nju.edu.cn>
2024-06-25 13:23:44 +0800

    change base dir

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ export default defineUserConfig({
 
   title: 'VuePress',
   description: 'My first VuePress Site',
+  base: '/BlogSite/',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
